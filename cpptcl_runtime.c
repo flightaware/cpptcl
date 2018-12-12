996f40ed4f1e254ff0932b18fac42aaae6309c50
snoe925 <snoe925@gmail.com>
2018-12-12 15:42:30 -0600

    Runtime (#15)
    * Add cpptcl_runtime
    
    It can be difficult to get the headers, tcl.h and cpptcl.h
    in correct order and settings when creating the first interpreter.
    The new global function
       Tcl_CreateInterpWithStubs(const char *version, int exact)
    make this easier as the library, cpptcl_runtime, is compiled without
    stubs.
    
    cpptcl end users can now get tcl.h from cpptcl.h.  They can also
    create interpreters by changing
    
    Tcl_CreateInterp() -> Tcl_CreateInterpWithStubs()
    
    and they can remote the initialization calls.
    
    * Add Tcl_CreateInterpWithStubs
    
    Add a helper function for TCL interpreter creation.
    
    * Change to static library.
    
    cpptcl_runtime is now static library.
    Add missing PIC flags to link.  No effect for static, but forgetting that
    is a hassle.

---
+++ cpptcl_runtime.c
@@ -0,0 +1,14 @@
+#include <tcl.h>
+
+Tcl_Interp * Tcl_CreateInterpWithStubs(const char *version, int exact)
+{
+    Tcl_Interp *interp = Tcl_CreateInterp();
+
+    if (Tcl_Init(interp) == TCL_ERROR
+        || Tcl_InitStubs(interp, version, exact) == NULL
+        || Tcl_PkgRequire(interp, "Tcl", version, exact) == NULL) {
+	return NULL;
+    }   
+
+    return interp;
+}
